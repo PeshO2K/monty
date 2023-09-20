40d85c0adc6b39cf0cffd9878a4d2ec5ab10eced
Patience Otuke <patienceotuke@gmail.com>
2023-09-20 23:13:14 +0300

    betty fix

--- ddlist_func.c
+++ ddlist_func.c
@@ -132,7 +132,7 @@ size_t print_dlistint(const stack_t *h, unsigned int top_only, strMode mode)
 }
 /**
  * print_string - Prints a string
- * @top: Pointer to the top of the stack
+ * 
  * @c: Integer c
  * @top_only: The top only
  *
@@ -145,11 +145,11 @@ int print_string(int c, unsigned int top_only)
 	if (top_only && (c < 0 || c > 127))
 	{
 		/*printf("debugger: out of range\n");*/
-		print_error(LINE, "can't %s, value out of range\n", stack.line_num, stack.opcode);
+		print_error(2, "can't %s, value out of range\n", stack.line_num, stack.opcode);
 	}
 	else if (c <= 0 || c > 127)
 	{
-			return (1);
+		return (1);
 	}
 	printf("%c", c);
 	return (top_only);
